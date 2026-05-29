96354baa4ce5fc7f03465420e7a0be97a3e7d2c2
sharan07-cmd <gopalan.sharan@gmail.com>
2026-05-29 09:53:39 +0000

    added comments

--- SPIDER_S3.py
+++ SPIDER_S3.py
@@ -6,25 +6,30 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 from cryptography.hazmat.primitives import padding
 
-def key_der(password,salt):
-    t=password.encode()
-    p=PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=500000)
-    key=p.derive(t)
+def key_der(password,salt):                                                          ## creating a function to add salt to the end of the password and scramble to make it to 32 bytes
 
-    return key
+    t=password.encode()                                                              ## converting the string to bytes
+    p=PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=500000)    ## calling the PBKDF2HMAC algorithm to add the salt to the end of the password, make it to the length of 32 and scramble it for 500000 times
+    key=p.derive(t)                                                                  ## passing the password bytes through the algorithm
 
-def aesencrypt(databytes,key,iv):
-    padder=padding.PKCS7(128).padder()
-    padded_data=padder.update(databytes)+padder.finalize()
-    cipher=cipher(algorithms.AES(key),modes.CBC(iv))
-    encryptor=cipher.encryptor()
-    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return key 
+
+def aesencrypt(databytes,key,iv):                                                    ## creating a function for encryption using AES in CBC mode
+
+    padder=padding.PKCS7(128).padder()                                               ## calling the PKCS7 algorithm so that padding can be done
+    padded_data=padder.update(databytes)+padder.finalize()                           ## making the data multiple of 16 bytes so that it can be passed through the AES
+    cipher=cipher(algorithms.AES(key),modes.CBC(iv))                                 ## calling the AES in CBC mode
+    encryptor=cipher.encryptor()                                                     ## Setting the AES to encryptor mode 
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()                ## passing the padded data through the AES ENCRYPTOR engine
+
     return ciphertext
 
-def aesdescrypt(ciphertext,key,iv):
-    cipher=Cipher(algorithms.AES(key),modes.CBC(iv))
-    decryptor= cipher.decryptor()
-    padded_data=decryptor.update(ciphertext)+decryptor.finalize()
-    unpadder=padding.PKCS7(128).unpadder()
-    original_text=unpadder.update(padded_data)+unpadder.finalize()
+def aesdescrypt(ciphertext,key,iv):                                                  ## creating a funciton for decryption using AES in CBC mode
+
+    cipher=Cipher(algorithms.AES(key),modes.CBC(iv))                                 ## calling the AES algorithm in CBC mode and passing the key and iv through it
+    decryptor= cipher.decryptor()                                                    ## setting it in decryption mode
+    padded_data=decryptor.update(ciphertext)+decryptor.finalize()                    ## passing the ciphertext through the decryption engine
+    unpadder=padding.PKCS7(128).unpadder()                                           ## Unpadding should be done to get the original text. So we are calling the PKCS7 algorithm in unpadding mode
+    original_text=unpadder.update(padded_data)+unpadder.finalize()                   ## passing the text with the padding
+
     return original_text
